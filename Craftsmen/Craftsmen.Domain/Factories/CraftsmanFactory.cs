7f6f176aa929ffd6933c98a2273dccc0c0a728f5
Georgi Petrov <78408524+p3trovbg@users.noreply.github.com>
2025-05-26 17:31:13 +0300

    add craftsman factory

---
+++ Craftsmen/Craftsmen.Domain/Factories/CraftsmanFactory.cs
@@ -0,0 +1,43 @@
+﻿using Craftsmen.Domain.Models;
+
+namespace Craftsmen.Domain.Factories;
+
+public class CraftsmanFactory : ICraftsmanFactory
+{
+    private string name = default!;
+    private string description = default!;
+    private string location = default!;
+    private Category category = default!;
+    private string userId = default!;
+
+    public ICraftsmanFactory WithName(string name)
+    {
+        this.name = name;
+        return this;
+    }
+    public ICraftsmanFactory WithDescription(string description)
+    {
+        this.description = description;
+        return this;
+    }
+
+    public ICraftsmanFactory WithLocation(string location)
+    {
+        this.location = location;
+        return this;
+    }
+
+    public ICraftsmanFactory WithCategory(Category category)
+    {
+        this.category = category;
+        return this;
+    }
+
+    public ICraftsmanFactory FromUser(string userId)
+    {
+        this.userId = userId;
+        return this;
+    }
+
+    public Craftsman Build() => new Craftsman(name, description, userId, location, category);
+}
---
+++ Craftsmen/Craftsmen.Domain/Factories/ICraftsmanFactory.cs
@@ -0,0 +1,17 @@
+﻿using Common.Domain.Contracts;
+
+using Craftsmen.Domain.Models;
+
+namespace Craftsmen.Domain.Factories;
+public interface ICraftsmanFactory : IFactory<Craftsman>
+{
+    ICraftsmanFactory WithName(string name);
+
+    ICraftsmanFactory WithDescription(string description);
+
+    ICraftsmanFactory WithLocation(string location);
+
+    ICraftsmanFactory WithCategory(Category category);
+
+    ICraftsmanFactory FromUser(string userId);
+}
