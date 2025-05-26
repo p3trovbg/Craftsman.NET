ee19f40f058ca66c2c1d65743e6cd35d75a37bee
Georgi Petrov <78408524+p3trovbg@users.noreply.github.com>
2025-05-26 17:32:00 +0300

    add craftsman validation and update options

--- Craftsmen/Craftsmen.Domain/Models/Category.cs
+++ Craftsmen/Craftsmen.Domain/Models/Category.cs
@@ -1,8 +1,24 @@
-﻿using Common.Domain.Models;
+﻿using Common.Domain;
+using Common.Domain.Models;
 
 namespace Craftsmen.Domain.Models;
 
 public class Category : Entity<int>
 {
-    
+    internal Category(string name, string description)
+    {
+        this.Validate(name, description);
+
+        this.Name = name;
+        this.Description = description;
+    }
+
+    public string Name { get; private set; }
+
+    public string Description { get; private set; }
+
+    private void Validate(string name, string description)
+    {
+        Guard.ForStringLength<>
+    }
 }
