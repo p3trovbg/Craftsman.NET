456560b98e319b14c3714d85c4b7efdca8757b95
Georgi Petrov <78408524+p3trovbg@users.noreply.github.com>
2025-05-28 23:08:42 +0300

    create category factory

---
+++ Craftsmen/Craftsmen.Domain/Factories/InnerFactories/CategoryFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace Craftsmen.Domain.Factories.InnerFactories;
+
+using Models;
+
+public class CategoryFactory
+{
+    private string name = default!;
+    private string? description = default!;
+
+    public CategoryFactory WithName(string name)
+    {
+        this.name = name;
+        return this;
+    }
+
+    public CategoryFactory WithDescription(string description)
+    {
+        this.description = description;
+        return this;
+    }
+
+    internal Category Build() => new Category(name, description);
+}
